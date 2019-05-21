1f80df9dd0d9330f53e25d8b0592a6e27ded647b
OddGamesSuen <49943198+KeeXian@users.noreply.github.com>
2019-05-21 21:53:16 +0800

    Update signup.php

--- signup.php
+++ signup.php
@@ -1,9 +1,13 @@
 <?php
+	//when user clicks the sign up button, he/she will be redirected to this page
 	session_start();
 		if(isset($_POST['signup-form'])){
 			$user = $_POST['Username'];
 			$password = $_POST['Password'];
-			$_SESSION[$user] = $password;
+			//the username and password is stored
+			//in the form of associative array, $_SESSION[username] = password
+			//similar to array(username=>password)
+			$_SESSION[$user] = $password;  
 			header("Location:Ver1.php?signup=success");
 		}
 		else{
