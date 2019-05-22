d791b1aa46365b72a47ca26253de84771dfafb8f
Night9Knight <49943191+Night9Knight@users.noreply.github.com>
2019-05-22 12:20:56 +0800

    Delete js1.js

--- js1.js
+++
@@ -1,9 +0,0 @@
-<script src="https://code.jquery.com/jquery-3.4.0.js" integrity="sha256-DYZMCC8HTC+QDr5QNaIcfR7VSPtcISykd+6eSmBW5qo=" crossorigin="anonymous"></script>
-<script type="text/javascript">
-  $(window).scroll(function() {
-    var scroll_position = $(window).scrollTop();
-    $('section').css({	//styling of image as section is css external file
-      'background-position-x': -scroll_position + 'px',	//parallax effect formula
-    })
-  })
-  </script>
